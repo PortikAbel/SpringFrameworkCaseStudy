ae5080402fb5431b916ecf2a12cb7d044628e205
PortikAbel <portikabel@yahoo.com>
2021-10-06 16:46:42 +0300

    Multi-modul Gradle alkalmazas

---
+++ paim1949-desktop/src/main/java/edu/bbte/idde/paim1949/frontend/HelloWorld.java
@@ -0,0 +1,12 @@
+package edu.bbte.idde.paim1949.frontend;
+
+import edu.bbte.idde.paim1949.backend.ExampleLibrary;
+import java.io.IOException;
+
+public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+
+        // külső saját modul függőségéből importált osztály
+        new ExampleLibrary().logHello();
+    }
+}
