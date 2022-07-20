9012501bb4a949f955a08014bebf6b4a4fbf0945
PortikAbel <portikabel@yahoo.com>
2022-07-20 11:47:47 +0300

    reactive deleted

--- paim1949-reactive/src/main/java/edu/bbte/idde/paim1949/backend/dao/RegionRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.bbte.idde.paim1949.backend.dao;
-
-import edu.bbte.idde.paim1949.backend.model.Region;
-import org.springframework.data.repository.reactive.ReactiveSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegionRepository extends ReactiveSortingRepository<Region, Long> {
-}
